071817b769dd0feed397b00997f9b94cd305fdab
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-06-03 14:10:21 +0000

    記事更新できるようになった

--- cms/src/Controller/ArticlesController.php
+++ cms/src/Controller/ArticlesController.php
@@ -56,4 +56,24 @@ class ArticlesController extends AppController
         }
         $this->set('article', $article);
     }
+
+    /**
+     * /articles/edit
+     *
+     * @param [type] $slug
+     * @return void
+     */
+    public function edit($slug)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        if ($this->request->is(['post', 'put'])) {
+            $this->Articles->patchEntity($article, $this->request->getData());
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been updated.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to update your article.'));
+        }
+        $this->set('article', $article);
+    }
 }
