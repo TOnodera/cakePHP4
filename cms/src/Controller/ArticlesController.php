2c7c727b4d4a81a8d21a69004f9755a1ee4ff9d8
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-06-03 14:01:22 +0000

    記事を新規登録できる

--- cms/src/Controller/ArticlesController.php
+++ cms/src/Controller/ArticlesController.php
@@ -4,6 +4,14 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+
+    public function initialize(): void
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+        $this->loadComponent('Flash'); // FlashComponent をインクルード
+    }
+
     /**
      * /articles/index
      *
@@ -26,5 +34,26 @@ class ArticlesController extends AppController
     {
         $article = $this->Articles->findBySlug($slug)->firstOrFail();
         $this->set(compact('article'));
+    }
+
+    /**
+     * post /articles/add
+     *
+     * @return void
+     */
+    public function add()
+    {
+        $article = $this->Articles->newEmptyEntity();
+        if ($this->request->is('post')) {
+            $article = $this->Articles->patchEntity($article, $this->request->getData());
+            // user_id の決め打ちは一時的なもので、あとで認証を構築する際に削除されます。
+            $article->user_id = 1;
+            if ($this->Articles->save($article)) {
+                $this->Flash->success(__('Your article has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('Unable to add your article.'));
+        }
+        $this->set('article', $article);
     }
 }
--- cms/src/Model/Table/ArticlesTable.php
+++ cms/src/Model/Table/ArticlesTable.php
@@ -2,12 +2,22 @@
 // src/Model/Table/ArticlesTable.php
 namespace App\Model\Table;
 
+use Cake\Event\EventInterface;
 use Cake\ORM\Table;
+use Cake\Utility\Text;
 
 class ArticlesTable extends Table
 {
     public function initialize(array $config): void
     {
         $this->addBehavior('Timestamp');
+    }
+
+    public function beforeSave(EventInterface $event, $entity, $options)
+    {
+        if ($entity->isNew() && !$entity->slug) {
+            $suggedTitle = Text::slug($entity->title);
+            $entity->slug = substr($suggedTitle, 0, 191);
+        }
     }
 }
