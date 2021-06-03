d125e1873cbb601071536300881fc38b79a6e575
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-06-03 13:41:39 +0000

    記事詳細が表示できる

--- cms/src/Controller/ArticlesController.php
+++ cms/src/Controller/ArticlesController.php
@@ -4,10 +4,27 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+    /**
+     * /articles/index
+     *
+     * @return void
+     */
     public function index()
     {
         $this->loadComponent('Paginator');
         $articles = $this->Paginator->paginate($this->Articles->find());
         $this->set(compact('articles'));
+    }
+
+    /**
+     * /articles/view 
+     *
+     * @param [type] $slug
+     * @return void
+     */
+    public function view($slug = null)
+    {
+        $article = $this->Articles->findBySlug($slug)->firstOrFail();
+        $this->set(compact('article'));
     }
 }
