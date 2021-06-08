e918894909bf02779475ffbef2dd94b87ee94c5d
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-06-08 12:29:32 +0900

    チュートリアル「タグとユーザー」まで実装

--- cms/src/Model/Entity/Article.php
+++ cms/src/Model/Entity/Article.php
@@ -2,6 +2,7 @@
 
 namespace App\Model\Entity;
 
+use Cake\Collection\Collection;
 use Cake\ORM\Entity;
 
 class Article extends Entity
@@ -9,6 +10,22 @@ class Article extends Entity
     protected $_accessiable = [
         '*' => true,
         'id' => false,
-        'slug' => false
+        'slug' => false,
+        'tag_string' => true
     ];
+
+    protected function _getTagString()
+    {
+        if (isset($this->_fields['tag_string'])) {
+            return $this->_fields['tag_string'];
+        }
+        if (empty($this->tags)) {
+            return '';
+        }
+        $tags = new Collection($this->tags);
+        $str = $tags->reduce(function ($string, $tag) {
+            return $string . $tag->title . ', ';
+        }, '');
+        return trim($str, ', ');
+    }
 }
